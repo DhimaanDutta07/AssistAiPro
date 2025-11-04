7d11560018008161c1764ce3c1fdd95d8c39a47a
Dhimaan Dutta <dhimaanmams.07@gmail.com>
2025-11-04 12:48:57 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -58,4 +58,4 @@ def route_leave_processor():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True) return entire corrected app.py without any comments
+    app.run(host='0.0.0.0', port=port, debug=True) 
